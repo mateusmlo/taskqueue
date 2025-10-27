a3c32cd32f86f7c31e21d616dc9c720bf3d28516
Mateus Mendonça <mateus.mlo95@gmail.com>
2025-10-27 19:56:20 -0300

    feat: generate inital grpc interface files

---
+++ proto/generate.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+export PATH="$HOME/go/bin:$PATH"
+protoc --go_out=. --go_opt=paths=source_relative \
+    --go-grpc_out=. --go-grpc_opt=paths=source_relative \
+    proto/taskqueue.proto
